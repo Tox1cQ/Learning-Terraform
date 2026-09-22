a869cdbeb644c78ad3733360dad8024d7eea7a0e
Tushar Chandak <tushar.chandak@godigitaltc.com>
2026-09-22 15:48:37 +0530

    Add Tags to terraform S3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "terraform_training" {
   bucket = "tushar-terraform-training-2026"
+
+  tags = {
+    Name        = "Terraform Training Bucket"
+    Environment = "Training"
+    ManagedBy   = "Terraform"
+  }
+
 }
