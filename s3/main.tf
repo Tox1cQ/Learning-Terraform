8d32c19bfd487131f8391c18a432a0b8a0bd7c33
Tushar Chandak <tushar.chandak@godigitaltc.com>
2026-09-22 16:22:24 +0530

    Made the bucket as a module

---
+++ s3/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "terraform_training" {
+  bucket = "tushar-terraform-training-2026-1"
+
+  tags = {
+    Name        = "Terraform Training Bucket"
+    Environment = "Training"
+    ManagedBy   = "Terraform"
+  }
+
+}
