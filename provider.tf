5a88f2f088f35f11a9bbe6d0626d79d17de212a9
Tushar Chandak <tushar.chandak@godigitaltc.com>
2026-09-22 18:31:30 +0530

    Main and provider with new modules

--- provider.tf
+++ provider.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>6.0"
+      version = "~> 6.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
     }
   }
 }
