1be64775193d331504e3aa823fa99cc0add01e64
Tushar Chandak <tushar.chandak@godigitaltc.com>
2026-09-22 16:23:38 +0530

    New main as a module

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "aws_s3_bucket" "terraform_training" {
-  bucket = "tushar-terraform-training-2026"
-
-  tags = {
-    Name        = "Terraform Training Bucket"
-    Environment = "Training"
-    ManagedBy   = "Terraform"
-  }
-
+module "s3" {
+  source = "./s3"
 }
